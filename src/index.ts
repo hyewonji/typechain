fb1a5277efc75939307d12e32a71af808d6d3c19
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-05-23 22:37:32 +0900

    Inteface on Typescript
    함수 인자에 해당하는 type을 interface를 통해 정의해준다.
    인자를 object로 보낼 수 있다.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 // 선언부터 export까지가 한 개의 모둘이다.
 
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "hyewon",
+    age: 20,
+    gender: "female"
+}
+
 // gender에 붙은 '?'가 gender인자가 optional이라는 것을 말해준다.
 // gender에 해당하는 인자가 없어도 에러를 발생시키지 않는다.
 
 // void : return 값이 없음을 명시
 // string : return 값이 문자열임을 명시
-const sayHi = (name: string, age:number, gender?:string):string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const sayHi = (person: Human):string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-console.log(sayHi("hyewon", 20, "female"));
+console.log(sayHi(person));
 
 export {};
